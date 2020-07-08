669296115694dfef84f874d5118ab8a5a4278f41
marcelomorales <lmoral10@jhu.edu>
2020-07-08 00:28:32 -0400

    Initial Commit: Basic structure and simple shape clases. Tests do not pass.

---
+++ src/com/jetbrains/Main.java
@@ -0,0 +1,12 @@
+package com.jetbrains;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	 write your code hereSystem.out.println(args[0]);
+
+        System.out.println("Hi everyone");
+    }
+    //ctrl r to run program
+}
+
